5e899a46398d78b242b6fd1cf1f7fc99cc02e431
Vincyyy_ <degapatirajvincy@gmail.com>
2026-02-19 15:09:59 +0530

    Add dark mode toggle and improve overall styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/lib/theme";
 import Index from "./pages/Index";
 
 const App = () => (
-  <>
+  <ThemeProvider defaultTheme="light" storageKey="password-checker-theme">
     <Sonner />
     <Index />
-  </>
+  </ThemeProvider>
 );
 
 export default App;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { PasswordChecker } from "@/components/PasswordChecker";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 const Index = () => {
   return (
@@ -7,6 +8,11 @@ const Index = () => {
         <div className="absolute -top-28 left-1/2 h-80 w-80 -translate-x-1/2 rounded-full bg-primary/15 blur-3xl" />
         <div className="absolute bottom-0 left-10 h-72 w-72 rounded-full bg-warning/10 blur-3xl" />
       </div>
+
+      <header className="relative mx-auto flex w-full max-w-5xl items-center justify-between">
+        <div></div>
+        <ThemeToggle />
+      </header>
 
       <div className="relative mx-auto flex min-h-[calc(100vh-6rem)] w-full max-w-5xl items-center justify-center">
         <PasswordChecker />
